1673c1e2018ac1a927f166b9265b0344863d9d1f
Philippe Giacinti <philippe@giacinti.com>
2021-09-01 21:09:39 +0200

    first clone if not installed already

--- docker/start.sh
+++ docker/start.sh
@@ -1,11 +1,21 @@
 #!/bin/sh
+DATA_PKI=/data/pki
+BIN_DIR=${DATA_PKI}/bin
+if [ ! -e ${BIN_DIR}/easypki ]; then
+    # install from gitub
+    cd ${DATA_PKI}
+    git clone https://github.com/giacinti/easypki.git
+    mv easypki/* .
+    rmdir easypki
+fi
 
-REALSTART=/data/pki/bin/start
+
+REALSTART=${BIN_DIR}/start
 
 if [ -x ${REALSTART} ]; then
     exec ${REALSTART}
 else
-    echo "no start script found in /data/pki/bin/start"
+    echo "no start script found in ${REALSTART}"
     echo "gracefully stopping... bye"
     exit 0
 fi
